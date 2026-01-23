58a161b5c2717cd1dd631b547a635d153b7fb84e
Alejandro Verde <ale.verde234@gmail.com>
2026-01-23 17:56:15 -0400

    V5

---
+++ dist/assets/js/config.js
@@ -0,0 +1,6 @@
+// CONFIGURACIÓN GLOBAL
+// Cambiar esta variable cuando subas el backend a producción (ej: Render, Railway)
+// En local: "http://localhost:5000"
+// En producción: "https://tu-api-en-render.com"
+
+const SERVER_URL = "http://localhost:5000";
