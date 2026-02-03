6483abb80c9550bc5863c4d3dfda7139bcaa0825
Alejandro Verde <ale.verde234@gmail.com>
2026-02-03 13:48:30 -0400

    V17

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get("/ping", (req, res) => {
 });
 
 // Puerto
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
